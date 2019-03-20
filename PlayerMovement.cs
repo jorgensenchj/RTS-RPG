02b8e668f8291f709ddcd655ee2b7f088e742ef3
Colin jrgensen <jorgensenchj22@gmail.com>
2019-03-20 08:41:25 -0700

    change

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
+        //change
         anim = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody>();
     }
